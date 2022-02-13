2a2cadbbe1d5824e3482f458783f6f8f0726cde0
KaiserWilheim <songyuxin060305@163.com>
2022-02-13 22:14:07 +0800

    20220213

---
+++ Luogu/p1500-p1999/p1500-1599/p1551/p1551.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main {
+    static int[] p;
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        int m=scan.nextInt();
+        int k=scan.nextInt();
+        p=new int[n+1];
+        for(int i=1;i<=n;i++){
+            p[i]=i;
+        }
+        for(int i=1;i<=m;i++){
+            int a=scan.nextInt();
+            int b=scan.nextInt();
+            int pa=find(a);
+            int pb=find(b);
+            if(pa!=pb){
+                p[pb]=pa;
+            }
+        }
+        for(int i=1;i<=k;i++){
+            int a=scan.nextInt();
+            int b=scan.nextInt();
+            int pa=find(a);
+            int pb=find(b);
+            if(pa==pb){
+                System.out.println("Yes");
+            }else{
+                System.out.println("No");
+            }
+        }
+    }
+    public static int find(int x){
+        if(p[x]!=x){
+            p[x]=find(p[x]);
+        }
+        return p[x];
+    }
+}
