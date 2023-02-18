c9928278df5534fde07f06e60b973e560ac19269
KaiserWilheim <songyuxin060305@gmail.com>
2023-02-18 19:49:11 +0800

    replace spaces to tabs

--- Luogu/p1000-p1999/p1057/p1057.py
+++ Luogu/p1000-p1999/p1057/p1057.py
@@ -4,11 +4,11 @@ n = int(r[0])
 m = int(r[1])
 f[0][1] = 1
 for i in range (1, m + 1):
-    for j in range (1, n + 1):
-        if j == 1:
-            f[i][j] = f[i - 1][n] + f[i - 1][2]
-        elif j == n:
-            f[i][j] = f[i - 1][1] + f[i - 1][n - 1]
-        else:
-            f[i][j] = f[i - 1][j - 1] + f[i - 1][j + 1]
+	for j in range (1, n + 1):
+		if j == 1:
+			f[i][j] = f[i - 1][n] + f[i - 1][2]
+		elif j == n:
+			f[i][j] = f[i - 1][1] + f[i - 1][n - 1]
+		else:
+			f[i][j] = f[i - 1][j - 1] + f[i - 1][j + 1]
 print(f[m][1])
